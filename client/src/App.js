287d719b03890101cbf22ae1564792c15a9f5906
Karsenh <KarsenHansen@gmail.com>
2021-03-27 03:43:54 -0700

    Removed DS_Store

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import './styles/App.css';
+import Sidebar from './components/Sidebar';
+
+function App() {
+  return (
+    <div className='app'>
+      <h1>Hey clever programmers... let's build a twitter clone.</h1>
+
+      {/* SIDEBAR */}
+      <Sidebar />
+
+      {/* FEED */}
+
+      {/* WIDGETS */}
+    </div>
+  );
+}
+
+export default App;
